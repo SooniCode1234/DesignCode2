89d32dcd07a09c759c92ee5f4fba941fe708e4b4
Sooni-Code1234 <sooni.mohammed@gmail.com>
2021-02-12 09:05:39 -0500

    Initial Commit

---
+++ DesignCode2/DesignCode2App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode2App.swift
+//  DesignCode2
+//
+//  Created by Sooni Mohammed on 2021-02-12.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
