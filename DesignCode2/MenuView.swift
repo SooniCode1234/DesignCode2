d9a18c32d0491a157453d05266de6a15f74e51dd
Sooni <sooni.dev@gmail.com>
2021-02-25 11:53:27 -0500

    Color Literal, Gradient and Overlay

--- DesignCode2/MenuView.swift
+++ DesignCode2/MenuView.swift
@@ -13,16 +13,42 @@ struct MenuView: View {
             Spacer()
             
             VStack(spacing: 16.0) {
+                Text("Meng - 28% completed")
+                    .font(.caption)
+                
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.08))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(12)
+                
                 MenuRow(title: "Account", icon: "gear")
                 MenuRow(title: "Billing", icon: "creditcard")
                 MenuRow(title: "Sign out", icon: "person.crop.circle")
             }
             .frame(maxWidth:  .infinity)
             .frame(height: 300)
-            .background(Color.white)
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(#colorLiteral(red: 0.8705882353, green: 0.8941176471, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom)
+            )
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(radius: 30)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .padding(.horizontal, 30)
+            .overlay(
+                Image("Avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            )
+            
+            
         }
         .padding(.bottom, 30)
     }
@@ -42,6 +68,7 @@ struct MenuRow: View {
         HStack(spacing: 16.0) {
             Image(systemName: icon)
                 .font(.system(size: 20, weight: .light))
+                .foregroundColor(Color(#colorLiteral(red: 0.66, green: 0.73, blue: 0.83, alpha: 1)))
                 .imageScale(.large)
                 .frame(width: 32, height: 32)
             
