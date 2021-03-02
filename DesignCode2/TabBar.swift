3506f8cdf383b09028379bf4c4978c4036d3b1c1
Sooni-Code1234 <sooni.mohammed@gmail.com>
2021-03-02 17:32:19 -0500

    Tab Bar and Test on Device

---
+++ DesignCode2/TabBar.swift
@@ -0,0 +1,39 @@
+//
+//  TabBar.swift
+//  DesignCode2
+//
+//  Created by Sooni Mohammed on 2021-03-02.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home()
+                .tabItem {
+                    Image(systemName: "play.circle.fill")
+                    Text("Home")
+                }
+            
+            ContentView()
+                .tabItem {
+                    Image(systemName: "rectangle.stack.fill")
+                    Text("Certificates")
+                }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TabBar()
+                .previewDevice("iPhone 8")
+            TabBar()
+                .previewDevice("iPhone 11 Pro Max")
+
+        }
+    }
+}
